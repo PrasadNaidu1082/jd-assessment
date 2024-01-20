621517b72b32d7ae30b9df60cc280402af2b8d06
PrasadNaidu1082 <36126926+PrasadNaidu1082@users.noreply.github.com>
2024-01-20 20:33:13 +0530

    Update cloudwatch.tf

--- infra/cloudwatch.tf
+++ infra/cloudwatch.tf
@@ -27,29 +27,3 @@ resource "aws_cloudwatch_metric_alarm" "scale_down" {
     AutoScalingGroupName = aws_autoscaling_group.jdoodle-asg
   }
 }
-
-
-resource "aws_cloudwatch_event_rule" "daily_refresh" {
-  name                = "daily_refresh"
-  schedule_expression = "cron(0 12 * * ? *)"  # Run every day at 12 am UTC
-}
-
-resource "aws_cloudwatch_event_target" "terminate_action" {
-  rule = aws_cloudwatch_event_rule.daily_refresh.name
-  arn  = aws_autoscaling_group.jdoodle-asg.id
-  role_arn = aws_iam_role.autoscaling_refresh_role.arn
-
-  input_transformer {
-    input_template = "{\"Action\":\"terminate\"}"
-  }
-}
-
-resource "aws_cloudwatch_event_target" "recreate_action" {
-  rule = aws_cloudwatch_event_rule.daily_refresh.name
-  arn  = aws_autoscaling_group.jdoodle-asg.id
-  role_arn = aws_iam_role.autoscaling_refresh_role.arn
-
-  input_transformer {
-    input_template = "{\"Action\":\"recreate\"}"
-  }
-}
