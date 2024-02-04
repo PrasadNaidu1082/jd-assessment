5f84613158654bbc1caf3af6b143f0d6ffc5ccd6
PrasadNaidu1082 <36126926+PrasadNaidu1082@users.noreply.github.com>
2024-02-04 14:49:44 +0530

    Update refreshevent.tf

--- infra/refreshevent.tf
+++ infra/refreshevent.tf
@@ -1,24 +1,44 @@
 resource "aws_cloudwatch_event_rule" "daily_refresh" {
-  name                = "daily_refresh"
-  schedule_expression = "cron(0 12 * * ? *)"  # Run every day at 12 am UTC
+  name = "daily-refresh"
+  description = "Daily refresh of Auto Scaling Group"
+  schedule_expression = "cron(0 0 * * ? *)"
 }
 
-resource "aws_cloudwatch_event_target" "terminate_action" {
+resource "aws_cloudwatch_event_target" "refresh_target" {
   rule = aws_cloudwatch_event_rule.daily_refresh.name
-  arn  = aws_autoscaling_group.jdoodle-asg.id
-  role_arn = aws_iam_role.autoscaling_refresh_role.arn
-
-  input_transformer {
-    input_template = "{\"Action\":\"terminate\"}"
-  }
+  target_id = "RefreshTarget"
+  arn = aws_lambda_function.refresh_lambda_function.arn
 }
 
-resource "aws_cloudwatch_event_target" "recreate_action" {
-  rule = aws_cloudwatch_event_rule.daily_refresh.name
-  arn  = aws_autoscaling_group.jdoodle-asg.id
-  role_arn = aws_iam_role.autoscaling_refresh_role.arn
+resource "aws_lambda_function" "refresh_lambda_function" {
+  function_name = "refreshlamdafunction"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  filename      = "refreshlamda_function.py"  
+}
 
-  input_transformer {
-    input_template = "{\"Action\":\"recreate\"}"
-  }
+resource "aws_iam_role" "lambda_role" {
+  name = "LambdaRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
+   name       = "lamda-policy-attachment"
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaBasicExecutionRole"
+  roles      = [aws_iam_role.lambda_role.name]
 }
