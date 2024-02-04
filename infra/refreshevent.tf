2d517ea6e05060d2224853f180dddeaabb986792
PrasadNaidu1082 <36126926+PrasadNaidu1082@users.noreply.github.com>
2024-02-04 16:08:40 +0530

    Update refreshevent.tf

--- infra/refreshevent.tf
+++ infra/refreshevent.tf
@@ -11,7 +11,7 @@ resource "aws_cloudwatch_event_target" "refresh_target" {
 }
 
 resource "aws_lambda_function" "refresh_lambda_function" {
-  function_name = "refreshlamdafunction"
+  function_name = "refreshlambdafunction"
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
