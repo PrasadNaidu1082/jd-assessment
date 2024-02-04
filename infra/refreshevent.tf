cb0a695f787600ce509e87ec7a1886828ed5a43a
PrasadNaidu1082 <36126926+PrasadNaidu1082@users.noreply.github.com>
2024-02-04 15:54:59 +0530

    Update refreshevent.tf

--- infra/refreshevent.tf
+++ infra/refreshevent.tf
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "refresh_lambda_function" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
-  filename      = "refreshlamda_function.py"  
+  filename      = "refreshlambda_function.zip"  
 }
 
 resource "aws_iam_role" "lambda_role" {
