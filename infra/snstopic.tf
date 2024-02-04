101524007b7b42a9479cc7f14e42e8363049903a
PrasadNaidu1082 <36126926+PrasadNaidu1082@users.noreply.github.com>
2024-02-04 14:53:45 +0530

    Update snstopic.tf

--- infra/snstopic.tf
+++ infra/snstopic.tf
@@ -7,3 +7,16 @@ resource "aws_sns_topic_subscription" "jdoodle_subscription" {
   protocol  = "email"
   endpoint  = "prasadnaidu1082@gmail.com"
 }
+
+resource "aws_sns_topic" "notification_topic" {
+  name = "instance_recreation_notifications"
+}
+
+resource "aws_cloudwatch_event_target" "notification_action" {
+  rule = aws_cloudwatch_event_rule.daily_refresh.name
+  arn  = aws_sns_topic.notification_topic.arn
+
+  input_transformer {
+    input_template = "{\"Message\":\"Instance deletion and recreation in progress.\"}"
+  }
+}
