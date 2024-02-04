ee8b31ec727a1b7de3d6005f1f23b5277068a20a
PrasadNaidu1082 <36126926+PrasadNaidu1082@users.noreply.github.com>
2024-02-04 14:48:54 +0530

    Update iam.tf

--- infra/iam.tf
+++ infra/iam.tf
@@ -15,12 +15,45 @@ resource "aws_iam_role" "cloudwatch_alarm_role" {
   })
 }
 
+resource "aws_iam_role" "autoscaling_refresh_role" {
+  name = "autoscaling-refresh"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "autoscaling.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "autoscaling_refresh_policy" {
+  name = "refresh-policy"
+  description = "Policy for refreshing Auto Scaling Group"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "autoscaling:UpdateAutoScalingGroup",
+        ],
+        Resource = aws_autoscaling_group.jdoodle-asg.arn
+      }
+    ]
+  })
+}
 resource "aws_iam_policy" "cloudwatch_alarm_policy" {
   name        = "CloudWatchAlarmPolicy"
   description = "IAM policy for CloudWatch Alarms to publish to SNS"
 
-  policy = jsonencode({ 
-    Version = "2012-10-17"  
+  policy = jsonencode({
+    Version = "2012-10-17"
     "Statement": [
     {
       "Effect": "Allow",
@@ -28,7 +61,7 @@ resource "aws_iam_policy" "cloudwatch_alarm_policy" {
         "SNS:Publish"
       ],
       "Resource": [
-        "${aws_sns_topic.jdoodle_subscription.arn}"
+        "${aws_sns_topic.jdoodle_topic.arn}"
       ]
     }
   ]
@@ -39,4 +72,9 @@ resource "aws_iam_policy" "cloudwatch_alarm_policy" {
 resource "aws_iam_role_policy_attachment" "cloudwatch_alarm_role_attachment" {
   policy_arn = aws_iam_policy.cloudwatch_alarm_policy.arn
   role       = aws_iam_role.cloudwatch_alarm_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "autoscaling_policy_attachment" {
+  policy_arn = aws_iam_policy.autoscaling_refresh_policy.arn
+  role = aws_iam_role.autoscaling_refresh_role.name
 }
